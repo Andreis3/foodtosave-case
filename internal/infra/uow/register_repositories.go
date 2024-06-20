cbe4181c5e34a4a1903d0f6620741f94b39f82f4
Andreis3 <andrei.as3@hotmail.com>
2024-06-20 15:52:47 -0300

    feat: register repositories

--- internal/infra/uow/register_repositories.go
+++ internal/infra/uow/register_repositories.go
@@ -3,7 +3,8 @@ package uow
 import (
 	"github.com/andreis3/foodtosave-case/internal/infra/adapters/db"
 	"github.com/andreis3/foodtosave-case/internal/infra/commons/observability"
-	repo_group "github.com/andreis3/foodtosave-case/internal/infra/repository/postgres/author"
+	"github.com/andreis3/foodtosave-case/internal/infra/repository/postgres/author"
+	"github.com/andreis3/foodtosave-case/internal/infra/repository/postgres/book"
 	"github.com/andreis3/foodtosave-case/internal/util"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -12,7 +13,12 @@ import (
 func NewProxyUnitOfWork(pool *pgxpool.Pool, metric observability.IMetricAdapter) *UnitOfWork {
 	uow := NewUnitOfWork(pool)
 	uow.Register(util.AUTH_REPOSITORY_KEY, func(tx any) any {
-		repo := repo_group.NewAuthorRepository(metric)
+		repo := author.NewAuthorRepository(metric)
+		repo.DB = db.New(tx.(pgx.Tx))
+		return repo
+	})
+	uow.Register(util.BOOK_REPOSITORY_KEY, func(tx any) any {
+		repo := book.NewBookRepository(metric)
 		repo.DB = db.New(tx.(pgx.Tx))
 		return repo
 	})
