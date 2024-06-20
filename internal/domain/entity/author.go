0b03b9c397e105c6e57367eab87f093debb93230
Andreis3 <andrei.as3@hotmail.com>
2024-06-20 07:41:36 -0300

    feat: implements validate author

--- internal/domain/entity/author.go
+++ internal/domain/entity/author.go
@@ -1,7 +1,20 @@
 package entity
 
+import "github.com/andreis3/foodtosave-case/internal/domain/notification"
+
 type Author struct {
 	ID          string
 	Name        string
 	Nationality string
+	notification.Error
+}
+
+func (a *Author) Validate() *notification.Error {
+	if a.Name == "" {
+		a.AddErrors(`name: is required`)
+	}
+	if a.Nationality == "" {
+		a.AddErrors(`nationality: is required`)
+	}
+	return &a.Error
 }
