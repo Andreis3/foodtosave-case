585764c2c9e686cf67ab4e09706c44ae1cdbe50c
Andreis3 <andrei.as3@hotmail.com>
2024-06-20 07:49:23 -0300

    feat: implements validate book

--- internal/domain/entity/book.go
+++ internal/domain/entity/book.go
@@ -1,7 +1,20 @@
 package entity
 
+import "github.com/andreis3/foodtosave-case/internal/domain/notification"
+
 type Book struct {
 	ID     string
 	Title  string
 	Gender string
+}
+
+func (b *Book) Validate() *notification.Error {
+	err := notification.NewError()
+	if b.Title == "" {
+		err.AddErrors(`title: is required`)
+	}
+	if b.Gender == "" {
+		err.AddErrors(`gender: is required`)
+	}
+	return err
 }
---
+++ tests/unit/domain/entity/book_test.go
@@ -0,0 +1,43 @@
+//go:build unit
+// +build unit
+
+package entity_test
+
+import (
+	"github.com/andreis3/foodtosave-case/internal/domain/entity"
+	"github.com/andreis3/foodtosave-case/internal/domain/notification"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("INTERNAL :: DOMAIN :: ENTITY :: BOOK", func() {
+	Describe("#Validate", func() {
+		Context("when the all fields is empty", func() {
+			It("should return error", func() {
+				book := &entity.Book{
+					ID:     "",
+					Title:  "",
+					Gender: "",
+				}
+				err := book.Validate()
+
+				Expect(err).NotTo(BeNil())
+				Expect(err.ReturnErrors()).To(HaveLen(2))
+				Expect(err.ReturnErrors()).To(ContainElement("title: is required"))
+				Expect(err.ReturnErrors()).To(ContainElement("gender: is required"))
+			})
+			It("should return an author valid ", func() {
+				book := &entity.Book{
+					ID:     "1",
+					Title:  "title",
+					Gender: "gender",
+				}
+
+				err := book.Validate()
+
+				Expect(err).To(Equal(&notification.Error{}))
+				Expect(err.ReturnErrors()).To(HaveLen(0))
+			})
+		})
+	})
+})
