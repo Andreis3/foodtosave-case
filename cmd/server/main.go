11d4535b36e4aec1f29b54c68dd1a4eb9dfbc75a
Andrei Santos <andrei.as3@hotmail.com>
2024-07-15 23:44:31 -0300

    chore: test folder commit personal

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/andreis3/foodtosave-case/internal/infra/common/configs"
 	"github.com/andreis3/foodtosave-case/internal/infra/common/logger"
 	"github.com/andreis3/foodtosave-case/internal/infra/server"
 	"github.com/andreis3/foodtosave-case/internal/util"
-	"os"
 )
 
 func main() {
 	log := logger.NewLogger()
 	conf, err := configs.LoadConfig(".")
 	if err != nil {
-		log.ErrorText(fmt.Sprintf("NotificationErrors loading config: %s", err.Error()))
+		log.ErrorText(fmt.Sprintf("Notification Errors loading config: %s", err.Error()))
 		os.Exit(util.EXIT_FAILURE)
 	}
 	server.Start(conf, log)
